f96184f048797636e7cba3270f86ad24057a179f
Benjamin Morgan <benmorgan2@gmail.com>
2015-08-20 21:10:21 +0100

    general cleaning up

--- symmetry/sitelist.py
+++ symmetry/sitelist.py
@@ -4,7 +4,9 @@ class SiteList( list ):
 
     @classmethod
     def read_from_file( cls, filename ):
+        """defines a SiteList instance by reading a set of coordinates x1, y1, z1, etc."""
         return cls( np.loadtxt( filename ).tolist() )
 
     def print_site( self, index ):
+        """prints the list of sites as x1, y1, z1, etc."""
         print( ' '.join( [ str(e) for e in self[ index ] ] ) )
