9bf14756a0ddaed36dc909eda90174e836d32832
Benjamin Morgan <benmorgan2@gmail.com>
2015-08-21 09:46:18 +0100

    Added docstrings to sitelist.py

--- symmetry/sitelist.py
+++ symmetry/sitelist.py
@@ -4,9 +4,21 @@ class SiteList( list ):
 
     @classmethod
     def read_from_file( cls, filename ):
-        """defines a SiteList instance by reading a set of coordinates x1, y1, z1, etc."""
+        """Generate a SiteList instance by reading a set of coordinates x1, y1, z1, etc.
+
+        Args:
+            filename (str): name of the file to be read.
+        Returns:
+            a new SiteList instance
+        """
         return cls( np.loadtxt( filename ).tolist() )
 
     def print_site( self, index ):
-        """prints the list of sites as x1, y1, z1, etc."""
+        """Print a subset of sites as x1, y1, z1, etc.
+
+        Args:
+            index (list): list of indices for which site coordinates to print
+        Returns:
+            None
+        """
         print( ' '.join( [ str(e) for e in self[ index ] ] ) )
