03d10734b30682311cc8ac214f45776686c4e293
Benjamin Morgan <benmorgan2@gmail.com>
2017-05-09 06:59:24 +0100

    Improved verbose text

--- bsym/pymatgen_interface.py
+++ bsym/pymatgen_interface.py
@@ -5,7 +5,7 @@ from pymatgen import Lattice, Structure
 from bsym.spacegroup import SpaceGroup
 from bsym import symmetry_operation as so
 
-def unique_symmetry_operations_as_vectors_from_structure( structure, subset = None ):
+def unique_symmetry_operations_as_vectors_from_structure( structure, verbose=True, subset=None ):
     """
     Uses pymatgen symmetry analysis to find the minimum complete set of symmetry operations for the space group of a structure.
 
@@ -17,11 +17,9 @@ def unique_symmetry_operations_as_vectors_from_structure( structure, subset = No
         a list of lists, containing the symmetry operations as vector mappings.
     """
     symmetry_analyzer = SpacegroupAnalyzer( structure )
-
-    print( "The spacegroup for structure is {}".format(symmetry_analyzer.get_space_group_symbol()) )
-
+    if verbose:
+        print( "The spacegroup for this structure is {}".format(symmetry_analyzer.get_space_group_symbol()) )
     symmetry_operations = symmetry_analyzer.get_symmetry_operations()
-
     mappings = []
     if subset:
         species_subset = [ spec for i,spec in enumerate(structure.species) if i in subset]
@@ -29,13 +27,11 @@ def unique_symmetry_operations_as_vectors_from_structure( structure, subset = No
         mapping_structure = Structure( structure.lattice, species_subset, frac_coords_subset ) 
     else:
         mapping_structure = structure
-
     for symmop in symmetry_operations:
         new_structure = Structure( mapping_structure.lattice, mapping_structure.species, symmop.operate_multi( mapping_structure.frac_coords ) )
         new_mapping = [ x+1 for x in list( coord_list_mapping_pbc( new_structure.frac_coords, mapping_structure.frac_coords ) ) ]
         if new_mapping not in mappings:
             mappings.append( new_mapping )
-
     return mappings
 
 def spacegroup_from_structure( structure, subset = None ):
