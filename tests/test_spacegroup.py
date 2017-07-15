69c5bea9f82864d6d8a3611ed511692d10ceb982
Benjamin Morgan <benmorgan2@gmail.com>
2017-07-15 15:11:30 +0100

    Finished docstrings and unittests for SpaceGroup.

--- tests/test_spacegroup.py
+++ tests/test_spacegroup.py
@@ -1,7 +1,8 @@
 import unittest
 from bsym import spacegroup
 from bsym import symmetry_operation as so
-from unittest.mock import Mock
+from unittest.mock import Mock, patch, call
+import numpy as np
 
 class SpaceGroupTestCase( unittest.TestCase ):
     """Tests for SpaceGroup class"""
@@ -12,5 +13,70 @@ class SpaceGroupTestCase( unittest.TestCase ):
         self.assertEqual( sg.symmetry_operations[0], s0 )
         self.assertEqual( sg.symmetry_operations[1], s1 )
 
+    def test_read_from_file( self ):
+        s0, s1 = Mock( spec=so.SymmetryOperation ), Mock( spec=so.SymmetryOperation )
+        with patch( 'numpy.loadtxt' ) as mock_np_loadtxt:
+            mock_np_loadtxt.return_value = np.array( [ [ 1, 2 ], [ 2, 1 ] ] )
+            with patch( 'bsym.symmetry_operation.SymmetryOperation.from_vector' ) as mock_from_vector:
+                mock_from_vector.side_effect = [ s0, s1 ]
+                sg = spacegroup.SpaceGroup.read_from_file( 'mock_filename' )
+                self.assertEqual( sg.symmetry_operations[0], s0 )
+                self.assertEqual( sg.symmetry_operations[1], s1 )
+                self.assertEqual( mock_from_vector.call_args_list[0], call( [ 1, 2 ] ) )
+                self.assertEqual( mock_from_vector.call_args_list[1], call( [ 2, 1 ] ) )
+
+    def test_read_from_file_with_labels( self ):
+        s0, s1 = Mock( spec=so.SymmetryOperation ), Mock( spec=so.SymmetryOperation )
+        with patch( 'numpy.genfromtxt' ) as mock_np_genfromtxt:
+            mock_np_genfromtxt.return_value = np.array( [ [ 'E', '1', '2' ], [ 'C2', '2', '1' ] ] )
+            with patch( 'bsym.symmetry_operation.SymmetryOperation.from_vector' ) as mock_from_vector:
+                mock_from_vector.side_effect = [ s0, s1 ]
+                sg = spacegroup.SpaceGroup.read_from_file_with_labels( 'mock_filename' )
+                self.assertEqual( sg.symmetry_operations[0], s0 )
+                self.assertEqual( sg.symmetry_operations[1], s1 )
+                self.assertEqual( mock_from_vector.call_args_list[0], call( [ 1, 2 ] ) )
+                self.assertEqual( mock_from_vector.call_args_list[1], call( [ 2, 1 ] ) )
+                self.assertEqual( s0.set_label.call_args, call( 'E' ) )
+                self.assertEqual( s1.set_label.call_args, call( 'C2' ) )
+    
+    def test_save_symmetry_operation_vectors_to( self ):
+        s0, s1 = Mock( spec=so.SymmetryOperation ), Mock( spec=so.SymmetryOperation )
+        s0.as_vector.return_value = [ 1, 2 ]
+        s1.as_vector.return_value = [ 2, 1 ]
+        sg = spacegroup.SpaceGroup( symmetry_operations=[ s0, s1 ] )
+        with patch( 'numpy.savetxt' ) as mock_savetxt:
+            sg.save_symmetry_operation_vectors_to( 'filename' ) 
+            self.assertEqual( mock_savetxt.call_args[0][0], 'filename' )
+            np.testing.assert_array_equal( mock_savetxt.call_args[0][1], np.array( [ [ 1, 2 ], [ 2, 1 ] ] ) )
+    
+    def test_extend( self ):
+        s0, s1 = Mock( spec=so.SymmetryOperation ), Mock( spec=so.SymmetryOperation )
+        sg = spacegroup.SpaceGroup( symmetry_operations=[ s0, s1 ] )
+        s2 = Mock( spec=so.SymmetryOperation)
+        sg.extend( [ s2 ] )
+        self.assertEqual( sg.symmetry_operations, [ s0, s1, s2 ] )
+       
+    def test_append( self ):
+        s0, s1 = Mock( spec=so.SymmetryOperation ), Mock( spec=so.SymmetryOperation )
+        sg = spacegroup.SpaceGroup( symmetry_operations=[ s0, s1 ] )
+        s2 = Mock( spec=so.SymmetryOperation)
+        sg.append( s2 )
+        self.assertEqual( sg.symmetry_operations, [ s0, s1, s2 ] )
+        	     
+    def test_by_label( self ):
+        s0, s1 = Mock( spec=so.SymmetryOperation ), Mock( spec=so.SymmetryOperation )
+        s0.label = 'A'
+        s1.label = 'B'
+        sg = spacegroup.SpaceGroup( symmetry_operations=[ s0, s1 ] )
+        self.assertEqual( sg.by_label( 'A' ), s0 )
+        self.assertEqual( sg.by_label( 'B' ), s1 )
+  
+    def test_labels( self ):
+        s0, s1 = Mock( spec=so.SymmetryOperation ), Mock( spec=so.SymmetryOperation )
+        s0.label = 'A'
+        s1.label = 'B'
+        sg = spacegroup.SpaceGroup( symmetry_operations=[ s0, s1 ] )
+        self.assertEqual( sg.labels, [ 'A', 'B' ] )
+  
 if __name__ == '__main__':
     unittest.main()
