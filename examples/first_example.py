cb5b3486907a35a7fd6868562031eb6545d638d4
Benjamin Morgan <benmorgan2@gmail.com>
2017-07-19 22:28:20 +0100

    Updated first_example to new API

--- examples/first_example.py
+++ examples/first_example.py
@@ -6,24 +6,26 @@
 # | 1 2 |
 # | 3 4 |
 
-from bsym import bsym as sym
+import bsym
+from bsym import process
 
 def square_lattice_example():
-    spacegroup_filename = '../spacegroups/cubic_spacegroup_annotated'
+    spacegroup_filename = '../spacegroups/square_spacegroup_annotated'
     # This contains a list of mappings between sites for all considered symmetry operations.
     # e.g. 
     # 1 2 3 4 (identity E: all sites unchanged)
     # 2 1 4 3 (reflection in a vertical plane: site 1 moves to 2 etc.)
     # 2 4 1 3 (90 degree rotation clockwise: site 1 moves to 2 etc.)
 
-    sg = sym.SpaceGroup.read_from_file( spacegroup_filename )
+    sg = bsym.SymmetryGroup.read_from_file( spacegroup_filename )
 
     # how many symmetry inequivalent ways can we occupy 2 sites in a 2x2 square periodic cell?
     # we define 2 occupation types with id '1' (occupied two sites) and 2 with id '0' (vacant two sites)
     site_dist = { 1 : 2, 
                   0 : 2 }
 
-    unique_configurations = sym.process.unique_configurations_from_sites( site_dist, sg, verbose=True )
+    cs = bsym.ConfigurationSpace( [ 'a', 'b', 'c', 'd' ], symmetry_group=sg ) 
+    unique_configurations = cs.unique_configurations( site_dist, verbose=True )
 
     [ config.pprint() for config in unique_configurations ] 
 
@@ -41,7 +43,4 @@ def square_lattice_example():
 
 if __name__ == '__main__':
     square_lattice_example()
-
-
-
 
