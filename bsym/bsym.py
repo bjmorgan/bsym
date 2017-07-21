2415ea2164c8f2f67451df7299507525980bc42f
Benjamin Morgan <benmorgan2@gmail.com>
2017-07-21 19:47:01 +0100

    Added warning on import bsym.bsym

---
+++ bsym/bsym.py
@@ -0,0 +1,6 @@
+import warnings
+import sys
+
+warnings.warn( "\nYou are trying to import bsym.bsym, which is not compatible with this version of bsym.\nPlease check the updated API information at https://github.com/bjmorgan/bsym,\n or download a compatible 0.1.* release at https://github.com/bjmorgan/bsym/releases.\n" )
+
+sys.exit()
