71733f5b2f623593de0f0d7e538b763e7de3f232
Benjamin Morgan <benmorgan2@gmail.com>
2017-07-21 19:41:09 +0100

    Removed sitelist and process modules

--- bsym/bsym.py
+++
@@ -1,5 +0,0 @@
-from .space_group import SpaceGroup
-from .configuration import Configuration
-from .sitelist import SiteList
-from .symmetry_operation import SymmetryOperation
-from . import process
