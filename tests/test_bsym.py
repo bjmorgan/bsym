f9154dc5c134a0be8fd3d09bc545e9c733e60d4c
Benjamin Morgan <benmorgan2@gmail.com>
2017-07-21 19:58:44 +0100

    Cleaned up top level bsym import tests

--- tests/test_bsym.py
+++ tests/test_bsym.py
@@ -1,23 +1,6 @@
 import unittest
 from unittest.mock import Mock
 
-class TestBsymImports( unittest.TestCase ):
-
-    def test_bsym_imports_SpaceGroup( self ):
-        from bsym.bsym import SpaceGroup
-
-    def test_bsym_imports_Configuration( self ):
-        from bsym.bsym import Configuration
-
-    def test_bsym_imports_SiteList( self ):
-        from bsym.bsym import SiteList
-
-    def test_bsym_imports_process( self ):
-        from bsym.bsym import process
-
-    def test_bsym_imports_SymmetryOperation( self ):
-        from bsym.bsym import SymmetryOperation
-
 class TestBsymTopLevelClasses( unittest.TestCase ):
 
     def test_bsym_imports_SymmetryOperation( self ):
