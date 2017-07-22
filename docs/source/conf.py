a534c9f6dda1e668f82b5f0e202757d0dc33ca9f
Benjamin Morgan <benmorgan2@gmail.com>
2017-07-22 23:50:15 +0100

    Some file renaming for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'bsym-docs'
+master_doc = 'index'
 
 # General information about the project.
 project = 'bsym'
