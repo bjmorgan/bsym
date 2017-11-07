ed2857979d55f14aed9779a776ea7a755f0a8d2d
Benjamin Morgan <benmorgan2@gmail.com>
2017-11-07 22:23:21 +0000

    Add unit tests

--- tests/test_colour_operation.py
+++ tests/test_colour_operation.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from bsym import ColourOperation
+from bsym import ColourOperation, Configuration
 from unittest.mock import patch
 
 class ColourOperationTestCase( unittest.TestCase ):
@@ -43,6 +43,27 @@ class ColourOperationTestCase( unittest.TestCase ):
         co = ColourOperation.from_vector( vector, mapping, count_from_zero=True )
         np.testing.assert_array_equal( co.matrix, np.matrix( [ [ 0, 0, 1 ], [ 1, 0, 0 ], [ 0, 1, 0 ] ] ) )
         self.assertEqual( co.colour_mapping, mapping )
+
+    def test_operate_on( self ):
+        matrix = np.matrix( [ [ 0, 1, 0 ], [ 0, 0, 1 ], [ 1, 0, 0 ] ] )
+        colour_mapping = [ { 1:1, 2:2, 3:3 },
+                           { 1:2, 2:3, 3:1 },
+                           { 1:3, 2:2, 3:1 } ]
+        co = ColourOperation( matrix, colour_mapping )
+        configuration = Configuration( [ 1, 2, 3 ] )
+        co.operate_on( configuration )
+        np.testing.assert_array_equal( co.operate_on( configuration ).vector, np.array( [ 2, 1, 3 ] ) )
+
+    def test_mul( self ):
+        matrix_a = np.array( [ [ 1, 1 ], [ 0, 0 ] ] )
+        colour_mapping_a = [ { 0:1, 1:0 }, { 0:1, 1:0 } ]
+        matrix_b = np.array( [ [ 1, 0 ], [ 1, 0 ] ] )
+        colour_mapping_b = [ { 0:1, 1:0 }, { 0:1, 1:0 } ]
+        co_a = ColourOperation( matrix_a, colour_mapping_a )
+        co_b = ColourOperation( matrix_b, colour_mapping_b )
+        co_c = co_a * co_b
+        np.testing.assert_array_equal( co_c.matrix , np.array( [ [ 2, 0 ], [ 0, 0 ] ] ) )
+        self.assertEqual( co_c.colour_mapping, [ { 0:0, 1:1 }, { 0:0, 1:1 } ] )
 
 if __name__ == '__main__':
     unittest.main()
