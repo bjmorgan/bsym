0248e14f3e7b3633a24e0d35856d892ddd1a3d71
Benjamin Morgan <benmorgan2@gmail.com>
2017-11-08 11:00:15 +0000

    Added unit tests for ConfigurationSpace

--- tests/test_configuration_space.py
+++ tests/test_configuration_space.py
@@ -1,7 +1,7 @@
 import unittest
-from unittest.mock import Mock
-from bsym import ConfigurationSpace, SymmetryGroup, SymmetryOperation
-from bsym.configuration_space import permutation_as_config_number
+from unittest.mock import Mock, patch
+from bsym import ConfigurationSpace, SymmetryGroup, SymmetryOperation, Configuration
+from bsym.configuration_space import permutation_as_config_number, colourings_generator
 import numpy as np
 
 class ConfigurationSpaceTestCase( unittest.TestCase ):
@@ -39,10 +39,52 @@ class ConfigurationSpaceTestCase( unittest.TestCase ):
         configuration_space = ConfigurationSpace( objects=object_list )
         self.assertEqual( issubclass( configuration_space.symmetry_group.symmetry_operations[0].matrix.dtype.type, np.integer ), True )
 
+    def test_unique_configurations( self ):
+        object_list = [ 1, 1, 2 ]
+        configuration_space = ConfigurationSpace( objects=object_list )
+        site_distribution = { 1:2, 2:1 }
+        mock_configuration = Mock( spec=Configuration )
+        configuration_space.enumerate_configurations = Mock( return_value=[ mock_configuration ] )
+        with patch( 'bsym.configuration_space.flatten_list' ) as mock_flatten_list:
+            mock_flatten_list.return_value = [ 1, 1, 2 ] 
+            with patch( 'bsym.configuration_space.unique_permutations' ) as mock_unique_permutations:
+                mock_unique_permutations.return_value = [ [ 1, 1, 2 ], [ 1, 2, 1 ], [ 2, 1, 1 ] ]
+                configurations = configuration_space.unique_configurations( site_distribution )
+                mock_unique_permutations.assert_called_with( [ 1, 1, 2 ] )
+            mock_flatten_list.assert_called_with( [ [1, 1 ], [ 2 ] ] )
+        configuration_space.enumerate_configurations.assert_called_with(
+                          mock_unique_permutations(), verbose=False )
+        self.assertEqual( configurations, [ mock_configuration ] )
+
+    def test_unique_colourings( self ):
+        object_list = [ 1, 1, 2 ]
+        configuration_space = ConfigurationSpace( objects=object_list )
+        mock_configuration = Mock( spec=Configuration )
+        mock_configuration.dim = 3
+        configuration_space.enumerate_configurations = Mock( return_value=[ mock_configuration ] )
+        with patch( 'bsym.configuration_space.colourings_generator' ) as mock_colourings_generator:
+            mock_colourings_generator.return_values = [ [ 1, 1, 2 ], [ 1, 2, 1 ], [ 2, 1, 1 ] ]
+            colourings = configuration_space.unique_colourings( colours=[ 1, 2 ] )
+            mock_colourings_generator.assert_called_with( [1, 2], mock_configuration.dim )    
+        configuration_space.enumerate_configurations.assert_called_with(
+                          mock_colourings_generator(), verbose=False )
+        self.assertEqual( colourings, [ mock_configuration ] )
+
 class ConfigurationSpaceModuleFunctionsTestCase( unittest.TestCase ):
       
     def test_permutation_as_config_number( self ):
         self.assertEqual( permutation_as_config_number( [ 1, 1, 0, 0, 1 ] ), 11001 )
 
+    def test_colourings_generator( self ):
+        colourings = list( colourings_generator( [ 1, 0 ], dim=3 ) )
+        expected_colourings = [ [1, 1, 1], 
+                                [0, 1, 1], [1, 0, 1], [1, 1, 0], 
+                                [0, 0, 1], [0, 1, 0], [1, 0, 0], 
+                                [0, 0, 0] ]
+        for c in colourings:
+            self.assertEqual( c in expected_colourings, True )
+        for ec in expected_colourings:
+            self.assertEqual( ec in colourings, True )
+        
 if __name__ == '__main__':
     unittest.main()
