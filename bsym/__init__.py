932d7439105ed014c274cced60bbd1fe2efb94af
Benjamin <b.j.morgan@bath.ac.uk>
2024-06-21 17:51:23 +0100

    Testing build fix

--- bsym/__init__.py
+++ bsym/__init__.py
@@ -1,11 +1,11 @@
-from .symmetry_operation import SymmetryOperation
-from .symmetry_group import SymmetryGroup
-from .space_group import SpaceGroup
-from .point_group import PointGroup
-from .configuration import Configuration
-from .configuration_space import ConfigurationSpace
-from .coordinate_config_space import CoordinateConfigSpace
-from .colour_operation import ColourOperation
+#from .symmetry_operation import SymmetryOperation
+#from .symmetry_group import SymmetryGroup
+#from .space_group import SpaceGroup
+#from .point_group import PointGroup
+#from .configuration import Configuration
+#from .configuration_space import ConfigurationSpace
+#from .coordinate_config_space import CoordinateConfigSpace
+#from .colour_operation import ColourOperation
 
 from .version import __version__
 
